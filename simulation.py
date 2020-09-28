f4b325a9bdfdd32e6fda026f3c490ff020a96efd
Tzvi Abel Chajet <achajet@gmail.com>
2020-09-28 12:43:33 -0400

    Added main() function.

--- simulation.py
+++ simulation.py
@@ -10,8 +10,7 @@ class queue:
 	def size(self):
 		return len(self.items)
 class server:
-	def __init__(self, proc_time):
-		self.proc_rate = proc_time
+	def __init__(self):
 		self.current_task = None
 		self.time_remaining = 0
 	def tick(self):
@@ -26,15 +25,42 @@ class server:
 			return False
 	def start_next(self,new_task):
 		self.current_task = new_task
-		self.time_remaining=new_task.get_pages()*60/self.page_rate
+		self.time_remaining=new_task.get_req()
 class request:
-	from random import randrange
-	def __init__(self, time):
-		self.timestamp = time
-		self.pages = randrange(1, 21)
+	def __init__(self, time, process_req):
+		self.timestamp=time
+		self.process_req=process_req
 	def get_stamp(self):
 		return self.timestamp
-	def get_pages(self):
-		return self.pages
+	def get_req(self):
+		return self.process_req
 	def wait_time(self, current_time):
-		return current_time - self.timestamp	
+		return current_time - self.timestamp
+def simulateOneServer(input_file):
+	from csv import reader
+	server_one=server()
+	s_queue=queue()
+	queue_time=[]
+	with open(input_file) as csv_file:
+		csv_requests=reader(csv_file)
+		for line in csv_requests:
+			job=request(int(line[0]),int(line[2]))
+			s_queue.enqueue(job)
+			if (not server_one.busy()) and (not s_queue.is_empty()):
+				next_task=s_queue.dequeue()
+				queue_time.append(next_task.wait_time(int(line[0])))
+				server_one.start_next(next_task)
+			server_one.tick()
+	csv_file.close()
+	average_wait = sum(queue_time)/len(queue_time)
+	print("Average Wait %6.2f secs %3d tasks remaining."%(average_wait, s_queue.size()))
+
+def main():
+	import argparse
+	parser=argparse.ArgumentParser()
+	parser.add_argument('--file', type=str)
+	args=parser.parse_args()
+	simulateOneServer(args.file)
+if __name__=="__main__":
+	main()	
+		
